ed6cc03c029736496dc36df781398563d2582063
EthanChenYen-Peng <ypcethan@gmail.com>
2020-07-11 11:43:35 +0800

    adding tweet input box;

---
+++ src/components/components/HomeTweetBox/HomeTweetBox.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
+import { faImage } from '@fortawesome/free-regular-svg-icons';
+import './HomeTweetBox.scss';
+
+const HomeTweetBox = () => (
+  <div className="home__tweet__box">
+    <div className="profile__icon">
+      <FontAwesomeIcon icon={faUser} />
+    </div>
+    <div className="home__tweet__container">
+      <input type="text" placeholder="What's happening?" className="home__tweet__input" />
+      <div className="home__tweet__buttons">
+        <div className="home__tweet__buttons__set">
+          <FontAwesomeIcon icon={faImage} className="home__tweet__icon" />
+        </div>
+        <button className="home__tweet__submit" type="button">
+          Tweet
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomeTweetBox;
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import HomeTweetBox from '../../components/HomeTweetBox/HomeTweetBox';
+import './Home.scss';
 
 const Home = () => (
-  <div>
-    Home
+  <div className="home">
+    <div className="home__title">
+      Home
+    </div>
+    <HomeTweetBox />
+    <hr className="home__divider" />
   </div>
 );
 
