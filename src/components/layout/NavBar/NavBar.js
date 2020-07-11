f388eb26857cf15bf02f1387c7b87de91b72d74b
EthanChenYen-Peng <ypcethan@gmail.com>
2020-07-11 09:16:54 +0800

    adding sidebar

--- src/components/layout/NavBar/NavBar.js
+++ src/components/layout/NavBar/NavBar.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import "./NavBar.scss"
-const NavBar = () => {
-    return (
-        <nav className='nav__container'>
-            <div className='nav__logo'>Logo</div>
-            <ul className='nav__list'>
-                <li className='nav__item nav__item--active'>Home</li>
-                <li className='nav__item'>Explore</li>
-                <li className='nav__item'>Messages</li>
-                <li className='nav__item'>Notifications</li>
-                <li className='nav__item'>Bookmarks</li>
-                <li className='nav__item'>Profile</li>
-            </ul>
-            <div>
-                Tweet
-            </div>
-        </nav>
-    )
-}
+import React from 'react';
+import './NavBar.scss';
 
-export default NavBar
+const NavBar = () => (
+  <nav className="nav__container">
+    <div className="nav__logo">Logo</div>
+    <ul className="nav__list">
+      <li className="nav__item nav__item--active">Home</li>
+      <li className="nav__item">Explore</li>
+      <li className="nav__item">Messages</li>
+      <li className="nav__item">Notifications</li>
+      <li className="nav__item">Bookmarks</li>
+      <li className="nav__item">Profile</li>
+    </ul>
+    <div>
+      Tweet
+    </div>
+  </nav>
+);
+
+export default NavBar;
--- src/components/layout/SideBar/SideBar.js
+++ src/components/layout/SideBar/SideBar.js
@@ -1,30 +1,42 @@
-import React,{useState} from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import "./SideBar.scss"
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import './SideBar.scss';
+
 const SideBar = () => {
-    const [searching,setSearching] = useState(false)
-    return (
-        <div className='side-bar__container'>
-           Side bar 
-           <div className={`side-bar__search ${searching ? "side-bar__search--active" : ""}`} >
-               <FontAwesomeIcon icon={faSearch} 
-                />
-               <input type="text"
-                placeholder='Search tweet'
-                className='side-bar__search__input'
-                onFocus={()=>setSearching(true)}
-                onBlur={()=>setSearching(false)}
-                />
-           </div>
-           <div className='side-bar__trend'>
-               <div className='title'>
-                    Trend for you
-
-               </div>
-           </div>
+  const [searching, setSearching] = useState(false);
+  return (
+    <div className="side-bar__container">
+      <div className={`side-bar__search ${searching ? 'side-bar__search--active' : ''}`}>
+        <FontAwesomeIcon icon={faSearch} />
+        <input
+          type="text"
+          placeholder="Search tweet"
+          className="side-bar__search__input"
+          onFocus={() => setSearching(true)}
+          onBlur={() => setSearching(false)}
+        />
+      </div>
+      <div className="side-bar__content__container">
+        <div className="title">
+          Trend for you
         </div>
-    )
-}
+        <ul className="side-bar__content__list">
+          <li className="side-bar__content__item">Tweet 1</li>
+          <li className="side-bar__content__item">Tweet 2</li>
+          <li className="side-bar__content__item">Tweet 3</li>
+        </ul>
+      </div>
+      <div className="side-bar__content__container">
+        <div className="title">Who to follow</div>
+        <ul className="side-bar__content__list">
+          <li className="side-bar__content__item">Tweet 1</li>
+          <li className="side-bar__content__item">Tweet 2</li>
+          <li className="side-bar__content__item">Tweet 3</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default SideBar
+export default SideBar;
