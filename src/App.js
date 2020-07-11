a2c70dbcc226dee200d7026af9de6fb4b7895ec6
EthanChenYen-Peng <ypcethan@gmail.com>
2020-07-11 09:32:57 +0800

    adding navbar icon

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import NavBar from './components/layout/NavBar/NavBar'
-import SideBar from './components/layout/SideBar/SideBar'
+import NavBar from './components/layout/NavBar/NavBar';
+import SideBar from './components/layout/SideBar/SideBar';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <div className='navbar__container'>
+      <div className="navbar__container">
         <NavBar />
       </div>
-      <div className='main__container'>
+      <div className="main__container">
         Main
       </div>
-      <div className='sidebar__container'>
-        <SideBar/>
+      <div className="sidebar__container">
+        <SideBar />
       </div>
     </div>
   );
--- src/components/layout/NavBar/NavBar.js
+++ src/components/layout/NavBar/NavBar.js
@@ -1,16 +1,44 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHome, faHashtag, faEnvelope, faBookmark, faUser,
+} from '@fortawesome/free-solid-svg-icons';
 import './NavBar.scss';
 
 const NavBar = () => (
   <nav className="nav__container">
     <div className="nav__logo">Logo</div>
     <ul className="nav__list">
-      <li className="nav__item nav__item--active">Home</li>
-      <li className="nav__item">Explore</li>
-      <li className="nav__item">Messages</li>
-      <li className="nav__item">Notifications</li>
-      <li className="nav__item">Bookmarks</li>
-      <li className="nav__item">Profile</li>
+      <li className="nav__item nav__item--active">
+        <FontAwesomeIcon icon={faHome} />
+        <span className="nav__item__title">
+          Home
+        </span>
+      </li>
+      <li className="nav__item">
+        <FontAwesomeIcon icon={faHashtag} />
+        <span className="nav__item__title">
+          Explore
+        </span>
+      </li>
+      <li className="nav__item">
+        <FontAwesomeIcon icon={faEnvelope} />
+        <span className="nav__item__title">
+          Messages
+        </span>
+      </li>
+      <li className="nav__item">
+        <FontAwesomeIcon icon={faBookmark} />
+        <span className="nav__item__title">
+          Bookmarks
+        </span>
+      </li>
+      <li className="nav__item">
+        <FontAwesomeIcon icon={faUser} />
+        <span className="nav__item__title">
+          Profile
+        </span>
+      </li>
     </ul>
     <div>
       Tweet
