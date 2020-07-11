116fef0c9e0567220a0a5a096293c5a7036a8c40
EthanChenYen-Peng <ypcethan@gmail.com>
2020-07-11 10:04:58 +0800

    adding responsive effect on navbar

--- src/components/layout/NavBar/NavBar.js
+++ src/components/layout/NavBar/NavBar.js
@@ -1,49 +1,45 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faHome, faHashtag, faEnvelope, faBookmark, faUser,
+  faHome,
+  faHashtag,
+  faEnvelope,
+  faBookmark,
+  faUser,
+  faFeather,
 } from '@fortawesome/free-solid-svg-icons';
 import './NavBar.scss';
 
 const NavBar = () => (
-  <nav className="nav__container">
+  <div className="nav__container">
     <div className="nav__logo">Logo</div>
     <ul className="nav__list">
       <li className="nav__item nav__item--active">
         <FontAwesomeIcon icon={faHome} />
-        <span className="nav__item__title">
-          Home
-        </span>
+        <span className="nav__item__title">Home</span>
       </li>
       <li className="nav__item">
         <FontAwesomeIcon icon={faHashtag} />
-        <span className="nav__item__title">
-          Explore
-        </span>
+        <span className="nav__item__title">Explore</span>
       </li>
       <li className="nav__item">
         <FontAwesomeIcon icon={faEnvelope} />
-        <span className="nav__item__title">
-          Messages
-        </span>
+        <span className="nav__item__title">Messages</span>
       </li>
       <li className="nav__item">
         <FontAwesomeIcon icon={faBookmark} />
-        <span className="nav__item__title">
-          Bookmarks
-        </span>
+        <span className="nav__item__title">Bookmarks</span>
       </li>
       <li className="nav__item">
         <FontAwesomeIcon icon={faUser} />
-        <span className="nav__item__title">
-          Profile
-        </span>
+        <span className="nav__item__title">Profile</span>
       </li>
     </ul>
-    <div>
-      Tweet
-    </div>
-  </nav>
+    <button type="button" className="tweet__container">
+      <FontAwesomeIcon icon={faFeather} className="feather__icon" />
+      <span className="tweet__title">Tweet</span>
+    </button>
+  </div>
 );
 
 export default NavBar;
