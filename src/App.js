67c0571403db7fcd8211a6a75577799246a81c1c
EthanChenYen-Peng <ypcethan@gmail.com>
2020-07-11 10:54:47 +0800

    adding react-router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NavBar from './components/layout/NavBar/NavBar';
 import SideBar from './components/layout/SideBar/SideBar';
+import Home from './components/pages/Home/Home';
+import Bookmarks from './components/pages/Bookmarks/Bookmarks';
+import Messages from './components/pages/Messages/Messages';
+import Profile from './components/pages/Profile/Profile';
 import './App.scss';
 
 function App() {
   return (
-    <div className="app">
-      <div className="navbar__container">
-        <NavBar />
+    <BrowserRouter>
+      <div className="app">
+        <div className="navbar__container">
+          <NavBar />
+        </div>
+        <div className="main__container">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/bookmarks" exact component={Bookmarks} />
+            <Route path="/messages" exact component={Messages} />
+            <Route path="/profile" exact component={Profile} />
+          </Switch>
+        </div>
+        <div className="sidebar__container">
+          <SideBar />
+        </div>
       </div>
-      <div className="main__container">
-        Main
-      </div>
-      <div className="sidebar__container">
-        <SideBar />
-      </div>
-    </div>
+
+    </BrowserRouter>
   );
 }
 
--- src/components/layout/NavBar/NavBar.js
+++ src/components/layout/NavBar/NavBar.js
@@ -9,34 +9,46 @@ import {
   faFeather,
   faDove,
 } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 import './NavBar.scss';
 
 const NavBar = () => (
   <div className="nav__container">
     <div className="nav__logo">
-
       <FontAwesomeIcon icon={faDove} />
     </div>
     <ul className="nav__list">
       <li className="nav__item nav__item--active">
-        <FontAwesomeIcon icon={faHome} />
-        <span className="nav__item__title">Home</span>
+        <Link to="/" className="nav__item__link">
+          <FontAwesomeIcon icon={faHome} />
+          <span className="nav__item__title">
+            Home
+          </span>
+        </Link>
       </li>
       <li className="nav__item">
-        <FontAwesomeIcon icon={faHashtag} />
-        <span className="nav__item__title">Explore</span>
+        <Link to="/explore" className="nav__item__link">
+          <FontAwesomeIcon icon={faHashtag} />
+          <span className="nav__item__title">Explore</span>
+        </Link>
       </li>
       <li className="nav__item">
-        <FontAwesomeIcon icon={faEnvelope} />
-        <span className="nav__item__title">Messages</span>
+        <Link to="/messages" className="nav__item__link">
+          <FontAwesomeIcon icon={faEnvelope} />
+          <span className="nav__item__title">Messages</span>
+        </Link>
       </li>
       <li className="nav__item">
-        <FontAwesomeIcon icon={faBookmark} />
-        <span className="nav__item__title">Bookmarks</span>
+        <Link to="/bookmarks" className="nav__item__link">
+          <FontAwesomeIcon icon={faBookmark} />
+          <span className="nav__item__title">Bookmarks</span>
+        </Link>
       </li>
       <li className="nav__item">
-        <FontAwesomeIcon icon={faUser} />
-        <span className="nav__item__title">Profile</span>
+        <Link to="/profile" className="nav__item__link">
+          <FontAwesomeIcon icon={faUser} />
+          <span className="nav__item__title">Profile</span>
+        </Link>
       </li>
     </ul>
     <button type="button" className="tweet__container">
