0b79dec1ccd6d03e2bd0aab3fd9e7d014ac7e69f
EthanChenYen-Peng <ypcethan@gmail.com>
2020-07-12 14:18:23 +0800

    adding profile page

--- src/components/pages/Profile/Profile.js
+++ src/components/pages/Profile/Profile.js
@@ -5,6 +5,23 @@ import './Profile.scss';
 const Profile = () => (
   <div className="profile__container">
     <ProfileHeader />
+    <div className="profile__content__container">
+      <div className="profile__image__container">
+        Image
+      </div>
+      <div className="profile__content">
+        Content
+        <div className="profile__content__top">
+          <div className="profile__content__avatar">
+            Avatar
+          </div>
+          <button type="button" className="profile__content__setup__btn"> Set up profile</button>
+        </div>
+        <div className="profile__content__bottom">
+          Button
+        </div>
+      </div>
+    </div>
   </div>
 );
 
